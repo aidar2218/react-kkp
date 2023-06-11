062fc1583d2f73df479fe7d95b007780bb6ad981
Aidar Ibraev <aidaribraev32@gmail.com>
2023-06-11 19:28:49 +0600

    Commit 02. Introduction, components, components ch.2, props, PropsType

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+type AccordionPropsType = {
+    title: string
+    collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+    console.log("Accordion rendering");
+
+    if (props.collapsed) {
+        return (
+            <div>
+                <AccordionTitle title={props.title}/>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <AccordionTitle title={props.title}/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering");
+    return (
+        <h3>---{props.title}---</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering");
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
