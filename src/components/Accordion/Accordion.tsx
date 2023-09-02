6676e999fe31273c5d0b9f14916db2c06541a2c6
Aidar Ibraev <aidaribraev32@gmail.com>
2023-09-02 19:12:26 +0600

    Revert "Commit 10, 11, 11.2. Storybook"
    This reverts commit 83f68427bac794d1fbc7c0128d3b7b464b4191ad.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -25,7 +25,7 @@ type AccordionTitlePropsType = {
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log("AccordionTitle rendering");
     return (
-        <h3 onClick={(e) => props.cb()}>---{props.title}---</h3>
+        <h3 onClick={props.cb}>---{props.title}---</h3>
     )
 }
 
