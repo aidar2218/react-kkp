1ad1cb43d5073e3f548615d15f42a03857a6f8ec
Aidar Ibraev <aidaribraev32@gmail.com>
2023-09-17 16:27:32 +0600

    Commit 15. Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,7 +1,5 @@
-// Button.stories.ts|tsx
 
-import type {Meta, StoryObj} from '@storybook/react';
-import {action} from "@storybook/addon-actions";
+import type {Meta} from '@storybook/react';
 
 import { Select } from './Select';
 import React, {useState} from "react";
@@ -14,17 +12,25 @@ export default meta;
 
 export type ItemType = {
     name: string
-    id: number
+    id: string
 }
 
 const selectArray: ItemType[] = [
-    {name: "Aidar", id: 1},
-    {name: "Azim", id: 2},
-    {name: "Baizak", id: 3},
+    {name: "Aidar", id: "1"},
+    {name: "Azim", id: "2"},
+    {name: "Baizak", id: "3"},
 ]
 
-export const FirstSelect = () => {
+export const WithValue = () => {
+    const [value, setValue] = useState("2");
     return (
-        <Select />
+        <Select onChange={setValue} value={value} items={selectArray}/>
+    )
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState<string | null>(null)
+    return (
+        <Select onChange={setValue} value={value} items={selectArray}/>
     )
 }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, {useState, KeyboardEvent, useEffect} from 'react';
+import {ItemType} from "./Select.stories";
+import s from "./Select.module.css"
 
-export const Select = () => {
+export type SelectPropsType = {
+    value?: string | null
+    onChange: (value: string) => void
+    items: ItemType[]
+}
+
+export const Select = (props: SelectPropsType) => {
+
+    const [active, setActive] = useState(true);
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+
+    const selectedItems = props.items.find(el => el.id === props.value);
+    const hoveredItem = props.items.find(el => el.id === hoveredElementValue);
+
+    const onSelectClick = () => {
+        setActive(!active);
+    };
+
+    const onItemClick = (id: string) => {
+        props.onChange(id);
+        onSelectClick();
+    };
+
+    useEffect(() => {
+        setHoveredElementValue(props.value);
+    }, [props.value])
+
+    const onKeyUpHandler = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].id === hoveredElementValue) {
+                    const pretendentElement = event.key === "ArrowDown"
+                        ? props.items[i + 1]
+                        : props.items[i - 1];
+                    if (pretendentElement) {
+                        props.onChange(pretendentElement.id);
+                        return;
+                    }
+                }
+            }
+            if (!selectedItems) {
+                props.onChange(props.items[0].id);
+            }
+        }
+        if (event.key === "Enter" || event.key === "Escape") {
+            setActive(false);
+        }
+    }
+
+
+
     return (
-        <div>
-
-        </div>
+        <>
+            <select>
+                <option value="">Bishkek</option>
+                <option value="">Astana</option>
+                <option value="">Tashkent</option>
+            </select>
+            <div className={s.select} onKeyUp={onKeyUpHandler} tabIndex={0}>
+                <span className={s.main} onClick={onSelectClick}>{selectedItems && selectedItems.name}</span>
+                {active && <div className={s.items}>
+                    {props.items.map(el => <div key={el.id}
+                                                onClick={() => onItemClick(el.id)}
+                                                className={s.item + " " + (hoveredItem === el ? s.selected : "")}
+                                                onMouseEnter={() => setHoveredElementValue(el.id)}
+                    >
+                        {el.name}
+                    </div>)}
+                </div>}
+            </div>
+        </>
     );
 };
 
